e0e325ca306f658e89c74e9bd354ba48b095157d
hgs-study <hgstudy_@naver.com>
2022-03-13 18:56:53 +0900

    CircuitBreaker Practice Initial commit

---
+++ src/test/java/com/circuitbreakerpractice/CircuitbreakerPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.circuitbreakerpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitbreakerPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
